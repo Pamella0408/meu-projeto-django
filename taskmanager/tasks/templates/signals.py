4ae77a954f31c74e8b4e10bd4a4f33f8b941e7d3
Pamella0408 <142030592+Pamella0408@users.noreply.github.com>
2024-09-15 20:49:02 -0300

    Update signals.py

--- taskmanager/tasks/templates/signals.py
+++ taskmanager/tasks/templates/signals.py
@@ -1,1 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Task
+from datetime import timedelta
+from django.utils import timezone
+
+@receiver(post_save, sender=Task)
+def task_deadline_notification(sender, instance, **kwargs):
+    if instance.deadline - timezone.now() <= timedelta(days=1):
+        # Exiba uma mensagem ou envie um email de notificação
+        print(f"Tarefa {instance.title} está próxima do vencimento!")
 
